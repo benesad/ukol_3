bbda3bdbfac48e24b054e86a824facda7159ee06
Adam <adam.bene98@gmail.com>
2021-01-26 15:20:59 +0100

    nastavení úsečky

--- Line.py
+++ Line.py
@@ -1,7 +1,25 @@
 from math import sqrt
 
 class Line:
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-        self.length = sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+    def __init__(self):
+        self.point1 = None
+        self.point2 = None
+        self.length = None
+
+    def set_point1(self, point):
+        self.point1 = point
+
+    def set_point2(self, point):
+        self.point2 = point
+
+    def get_length(self):
+        if self.length != None:
+            return self.length
+        self.length = sqrt((self.point1.x - self.point2.x)**2 + (self.point1.y - self.point2.y)**2)
+        return self.length
+
+    def is_set_point2(self):
+        return self.point2 != None
+
+    def is_set_point1(self):
+        return self.point1 != None
--- Polyline.py
+++ Polyline.py
@@ -1,4 +1,11 @@
+from Point import Point
+from Line import Line
+
 class Polyline:
     def __init__(self, data_of_points):
+        self.lines = []
+        tmpLine = Line()
+        
         for data_point in data_of_points:
+            point = Point(data_point[0], data_point[1])
             
